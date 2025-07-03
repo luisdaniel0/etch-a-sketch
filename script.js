53305c1b31f786de1a61d5ce8ba750b24872dce3
Luis Sanchez <luisanchezdh@gmail.com>
2025-07-03 18:03:25 -0400

    added a <div> container and appended 256 other divs to it with a for loop

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const container = document.querySelector('.container')
+
+const newDivs = document.createElement('div')
+
+
+
+container.appendChild(newDivs)
+
+for(let i=0;i<256; i++){
+    const squareDiv = document.createElement('div')
+    squareDiv.classList.add('grid')
+    container.appendChild(squareDiv)
+}
