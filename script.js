72897afc1e8b25bf39d185745ede9a9edd48e267
Luis Sanchez <luisanchezdh@gmail.com>
2025-07-08 16:25:05 -0400

    added event listener to grid

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ for(let i=0;i<256; i++){
     const squareDiv = document.createElement('div')
     squareDiv.classList.add('grid')
     container.appendChild(squareDiv)
+    squareDiv.addEventListener("mouseenter",()=>{
+        squareDiv.style.backgroundColor="black"
+    })
 }
