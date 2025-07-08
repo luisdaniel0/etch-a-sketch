ea0484e45761a8364b393746f4cb8b9cb3c554e2
Luis Sanchez <luisanchezdh@gmail.com>
2025-07-08 16:15:40 -0400

    looped div 256 times

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const container = document.querySelector('.container')
 
-const newDivs = document.createElement('div')
-
-
-
-container.appendChild(newDivs)
-
 for(let i=0;i<256; i++){
     const squareDiv = document.createElement('div')
     squareDiv.classList.add('grid')
